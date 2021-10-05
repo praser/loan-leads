201c17479e790b15e40ae86adca36b41ec5e8cc4
Rubens Praser Júnior <praser@gmail.com>
2021-10-05 19:27:35 -0300

    Configure typeorm

---
+++ packages/database/ormconfig.ts
@@ -0,0 +1,22 @@
+const { DB_LANG, DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env
+
+const config = {
+  type: DB_LANG,
+  host: DB_HOST,
+  port: DB_PORT,
+  username: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+}
+
+module.exports = config
